a0df7e3a84d9c7a42dbc2756362fdfee0bfa69fe
TWOdunlami <t_alvarez88@hotmail.com>
2021-12-10 01:42:47 -0500

    code lines 2-8: functions addedd to the 'Get Started' and 'pricing tier select' buttons have onclick functions.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function inactiveFunc() {
-    
+    alert("Inactive selection. Thank you for viewing yourNotebook📑landing page");
 }
+
+// Link below will be changed to a tradional 404-error page template.
+function userFunc() {
+    window.open("https://www.freecodecamp.com/email-submit)", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+  }
