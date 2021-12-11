dc29313094f65558fc2a108e1835cd470b19a1d4
TWOdunlami <t_alvarez88@hotmail.com>
2021-12-11 05:05:59 -0500

    code line 7: updated for deployment usability.

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ function inactiveFunc() {
 
 // Link below will be changed to a tradional 404-error page template.
 function userFunc() {
-    window.open("https://www.freecodecamp.com/email-submit)", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+    window.open("https://www.freecodecamp.com/email-submit", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
   }
