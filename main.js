852819d22b2659642155fdfb5a39cae787888000
TWOdunlami <t_alvarez88@hotmail.com>
2022-01-27 14:53:18 -0500

    code line 7: user is not re-routed to fill out survey. temporary.

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ function inactiveFunc() {
 
 // Link below will be changed to a tradional 404-error page template.
 function userFunc() {
-    window.open("https://www.freecodecamp.com/email-submit", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+    window.open("https://lrydatabuilders.w3spaces.com/GIT/quoteSurvey.html?bypass-cache=13132881", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
   }
